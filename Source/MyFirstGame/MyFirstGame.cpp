51ff3fedfe11fe4653d407f1a378ffdd65be4361
IsArnoAh <2286430915@qq.com>
2023-04-13 09:51:43 +0800

    初始化项目

---
+++ Source/MyFirstGame/MyFirstGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyFirstGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyFirstGame, "MyFirstGame" );
